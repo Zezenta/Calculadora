7f3532edf5ca50b30cf87e7801b3715732583f98
Marco <marcoadrianzambra@gmail.com>
2024-08-12 23:13:31 -0500

    función de multiplicación y división con jerarquía

--- code.js
+++ code.js
@@ -51,38 +51,49 @@ const identificarNumeros = (event) => {
             case '=':
                 filaDeNumeros.unshift('=');
                 numerosOperables.push(filaDeNumeros.slice())
+                // console.log(filaDeNumeros);
                 filaDeNumeros.length = 0;
-                console.log(numerosOperables);
-                console.log(checkOp(numerosOperables, '+'));
                 
-
+                console.log(numerosOperables);
+                // console.log(checkOp(numerosOperables, '+'));
+                
                 //Se itera solo cuando exista las siguientes operaciones pendientes.
-                while (numerosOperables.length > 0) {
+                while (numerosOperables.length > 1) {
                     //verificamos primeramente si tenemos operaciones de mult y div, para ejecutarlas primero, segun jerarquia.
                     if (checkOp(numerosOperables, 'x') || checkOp(numerosOperables, '÷')) {
+                        //iteramos las filas del array al reves, es decir, de la ultima a la primera
                         for (let i = numerosOperables.length - 1; i >= 0; i--) {
                             if (numerosOperables[i][0] === 'x') {
-                                let multiplicando = multiplicar(numerosOperables[i], numerosOperables[i+1])
-                                console.log(multiplicando);
-                                
+                                //guardamos la operacion que va a seguir en el array resultado
+                                let aux = numerosOperables[i+1][0];     //guardamos la operacion que va a seguir en el array resultado
+                                //Ejecutamos la operacion, y obtenemos el resultado en un array, igual que todo lo demas
+                                let multiplicando = multiplicar(numerosOperables[i], numerosOperables[i+1]);
+                                //funcion que convierte mi numero, a array y lo coloca el array 2d, remplazando los dos anteriores
+                                fromNumberToArray(i, multiplicando, aux);
                             } else if (numerosOperables[i][0] === '÷') {
-                                let dividiendo = dividir(numerosOperables[i], numerosOperables[i+1])
-                                console.log(dividiendo);
-                                
+                                //Aca vamos a hacer lo mismo que en la multiplicacion
+                                let aux = numerosOperables[i+1][0]; 
+                                let dividiendo = dividir(numerosOperables[i], numerosOperables[i+1]);
+                                fromNumberToArray(i, dividiendo, aux);
                             }
                         }
                     } else {
+                        //iteramos las filas del array al reves, es decir, de la ultima a la primera
                         for (let i = numerosOperables.length - 1; i >= 0; i--) {
                             if (numerosOperables[i][0] === '+') {
                                 let sumando = sumar(numerosOperables[i], numerosOperables[i+1])
+                                console.log(sumando);
 
                             } else if (numerosOperables[i][0] === '-') {
                                 let restando = restar(numerosOperables[i], numerosOperables[i+1])
-                                
+                                console.log(restando);
                             }
                         }
                     }
                 }
+
+                console.log('si acabo');
+                
             break;
         }
     }
@@ -93,7 +104,7 @@ function checkOp(array, valor) {
     return array.some(subArray => subArray.includes(valor));
 }
 
-
+//convertir nuestro array en un numero para poder operar
 function fromArrayToNumber(op1, op2) {
     //quitamos el primer elemento, que es el identificador de operacion, y solo dejamos las cantidades
     op1.shift()
@@ -101,10 +112,18 @@ function fromArrayToNumber(op1, op2) {
     //unimos el array de strings sin espacios, y lo convertimos con la funcion Number, que funciona para ints y floats.
     op1 = Number(op1.join(''));
     op2 = Number(op2.join(''));
-
+    
     return {op1, op2};
 }
-
+//convertir nuestro numero en array, para poder guardarlo en el array 2d
+function fromNumberToArray(index, number, aux) {
+    //convertimos nuestro numero en string, y a su vez lo hacemos un array dividiendolo por cada componente que tenga.
+    number = number.toString().split('');
+    number.unshift(aux);
+    console.log(numerosOperables);
+    //remplazamos los dos arrays anteriores, y lo metemos el nuevo donde debe de estar
+    numerosOperables.splice(index, 2, number);
+}
 
 //funciones de operaciones
 function sumar(op1, op2) {
