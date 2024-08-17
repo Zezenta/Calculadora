694a4c0d7d39473eb584723f65a5ec10f47247e3
Marco <marcoadrianzambra@gmail.com>
2024-08-17 18:06:09 -0500

    Implementación de borrar uno por uno y optimzamos

--- code.js
+++ code.js
@@ -3,37 +3,59 @@ const textoPantalla = document.querySelector('.container__textoPantalla') //etiq
 
 let numerosOperables = [];  //array 2d vacio, que va a contener todos los numeros a operar, junto con su identificador de operacion
 let filaDeNumeros = []; //array individual que tiene los numeros a operar, que se almacena en el array 2d
-let operadores = ['x', '+', '-', '%', '÷'];
-let operado = false;
+let operadores = ['x', '+', '-', '%', '÷', 'AC', 'C'];
 
 //funcion que se ejecuta cuando apretamos un boton
 const identificarNumeros = (event) => {
-    //agregamos el texto
-    if (operadores.includes(event.target.textContent)) {
-        textoPantalla.textContent += ` ${event.target.textContent} `;
-    } else {
-        textoPantalla.textContent += event.target.textContent;
-    }
 
     //verificamos que el contenido del event sea un numero, en caso de no serlo no se hace push a la fila de numeros
     if (!isNaN(parseInt(event.target.textContent))) {
         filaDeNumeros.push(event.target.textContent);
+        textoPantalla.textContent += event.target.textContent;
+    }
+
+    //Evaluamos condiciones para los botones de borrar y como se agrega el texto
+    switch (true) {
+        //Borramos la info de todos los arrays, y lo que aparece en el texto
+        case event.target.textContent === 'AC':
+            filaDeNumeros.length = 0;
+            numerosOperables.length = 0;
+            textoPantalla.textContent = '';
+            break;
+        
+        //Borramos uno por uno
+        case event.target.textContent === 'C':
+            //Condicion para borrar la fila que escribimos antes de designarle un operador
+            if (filaDeNumeros.length > 0) {
+                filaDeNumeros.pop();
+            
+            } else if(filaDeNumeros.length === 0 && numerosOperables.length > 0){   //Siguiente array, es el operador
+                numerosOperables[numerosOperables.length - 1].shift();  //eliminamos el operador del ultimo array de nuestro array 2d
+
+                for(let element of numerosOperables[numerosOperables.length - 1]){
+                    filaDeNumeros.push(element);    //Todos los numeros del ultimo array, se van de nuevo a la fila de numeros (Restart)
+                }
+
+                numerosOperables.pop();//Eliminamos el ultimo elemento de nuestro array 2d, nunca existio
+
+                //Borramos en pantalla el operador anterior, junto con sus espacios que le habiamos dejado
+                textoPantalla.textContent = textoPantalla.textContent.slice(0, -2);
+            }
+
+            //Eliminamos el ultimo elemento de lo que tenemos en nuestra etiqueta p
+            textoPantalla.textContent = textoPantalla.textContent.slice(0, -1); 
+            break;
+        
+        //En caso de ser un operador, dar espacios al momento de escribir
+        case operadores.includes(event.target.textContent):
+            textoPantalla.textContent += ` ${event.target.textContent} `;
+            break;
     }
     
-    //Ejecutamos solo cuando tenemos al menos un numero en el array
+    //Ejecutamos solo cuando tenemos al menos un numero por operar
     if (filaDeNumeros.length > 0) {
-        // console.log(event.target.textContent);
-        
         //verificamos que boton de operacion apretamos, segun eso hacemos una accion u otra
         switch (event.target.textContent) {
-            case 'C':
-
-                break;
-            case 'AC':
-                filaDeNumeros.length = 0;
-                numerosOperables.length = 0;
-                textoPantalla.textContent = '';
-                break;
             case '%':
                 filaDeNumeros.unshift('%');
                 numerosOperables.push(filaDeNumeros.slice())
@@ -48,7 +70,6 @@ const identificarNumeros = (event) => {
                 filaDeNumeros.unshift('x');
                 numerosOperables.push(filaDeNumeros.slice())
                 filaDeNumeros.length = 0;
-                console.log(numerosOperables);
                 break;
             case '-':
                 filaDeNumeros.unshift('-');
@@ -63,11 +84,11 @@ const identificarNumeros = (event) => {
             case ',':
                 break;
             case '=':
-                filaDeNumeros.unshift('=');
+                filaDeNumeros.unshift('='); //Para identificar que esa es la ultima fila a operar
                 numerosOperables.push(filaDeNumeros.slice())
                 filaDeNumeros.length = 0;
                 
-                //Se itera solo cuando exista las siguientes operaciones pendientes.
+                //Se itera solo cuando exista las siguientes operaciones pendientes.    (logica de jerarquia de numeros)
                 while (numerosOperables.length > 1) {
                     //verificamos primeramente si tenemos operaciones de mult y div, para ejecutarlas primero, segun jerarquia.
                     if (checkOp(numerosOperables, 'x') || checkOp(numerosOperables, '÷')) {
@@ -104,17 +125,17 @@ const identificarNumeros = (event) => {
                         }
                     }
                 }
-                // console.log(numerosOperables);
-                numerosOperables[0].shift();
-                for(let element of numerosOperables[0]){
-                    filaDeNumeros.push(element);
+
+                numerosOperables[0].shift();    //Eliminamos el operador '='
+
+                for(let element of numerosOperables[0]){    //Iteramos para que el resultado, se guarde a fila de numeros (Restart)
+                    filaDeNumeros.push(element);    
                 }
                 
-                let resultado = Number(numerosOperables[0].join(''));
+                //Convertimos nuestro array a numero, y lo mostramos
+                let resultado = Number(numerosOperables[0].join('')); 
                 textoPantalla.textContent = resultado;
-                numerosOperables.length = 0;
-                operado = true;
-                
+                numerosOperables.length = 0;    //Vaciamos el contendio del array 2d.
             break;
         }
     }
@@ -173,11 +194,13 @@ botones.forEach(boton => {
 
 
 
-// Cosas que faltan
+// Cosas que faltan:
 // Arreglar como se distribuye el texto en la pantalla
 // Aprender a como sacar porcentajes
-// Borrar cantidades u operadores uno por uno
 // Implementar las comas para poder escribir numeros decimales
+// Si pongo un numero varios operadores iguales, se hace la esa igual
+// si pongo un numero y diferentes operadores, se toma como valido el primero 
 
-// OPCIONAL
-// Implementacion de visualizacion en vertical, con mas funcions 
+// OPCIONAL:
+// Implementar poder colocar los numeros con los botones del teclado
+// Si es que se puede, buscarle lugar a usar parentesis, eso desenglosaria un poco de problemas mas grandes
